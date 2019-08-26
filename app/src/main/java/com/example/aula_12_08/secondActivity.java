06fa6648146ca8372dfcbd8bc13ca6623ac1429b
BrunoRenosto <brunovitor.bvr@gmail.com>
2019-08-26 15:09:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aula_12_08/secondActivity.java
@@ -0,0 +1,14 @@
+package com.example.aula_12_08;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.edu.ifc.concordia.hewerton.basicexplictintent.R;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+}
